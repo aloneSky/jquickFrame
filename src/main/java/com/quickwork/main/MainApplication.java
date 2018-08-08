ec9b8c36e0001eb051067b422a60775de6aa44be
Administrator <Administrator@PC-20170711XNFW>
2018-08-08 10:17:25 +0800

    se

--- src/main/java/com/quickwork/main/MainApplication.java
+++ src/main/java/com/quickwork/main/MainApplication.java
@@ -1,5 +1,4 @@
 package com.quickwork.main;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
---
+++ src/main/java/com/quickwork/main/sys/user/dao/UserDao.java
@@ -0,0 +1,8 @@
+package com.quickwork.main.sys.user.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface UserDao {
+	public void find();
+}
